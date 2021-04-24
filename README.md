4c9a00822b4927456ed590b6969978868000bbf0
ragpanda <wjt233800681@gmail.com>
2021-04-24 16:30:00 +0800

    feat: add readme, bench

--- README.md
+++ README.md
@@ -2,3 +2,37 @@
 A lightweight future, allows users to better abstract concurrency logic.
 
 ![Go Report Card](https://goreportcard.com/badge/github.com/ragpanda/light-future) ![Build status](https://travis-ci.com/ragpanda/light-future.svg?branch=master)
+
+
+
+# Intro
+Using Future tends to be more readable, asynchronous behavior is abstracted under the Future, and code tends to be more elegant
+
+Example
+```golang
+
+    result, err := NewFuture(ctx, NewClosureRunnable(func(ctx context.Context) (interface{}, error) {
+        b := a
+        return b, nil
+    })).
+    Send().
+    Await().
+    Result()
+
+```
+
+
+# Benchmark
+
+Compare using future and pass the result by the channel, you can compare this cost to determine whether to use
+
+```
+goos: linux
+goarch: amd64
+pkg: github.com/ragpanda/light-future
+cpu: Intel(R) Core(TM) i7-6700K CPU @ 4.00GHz
+BenchmarkFuture-8                        	   10000	    107847 ns/op	   44655 B/op	     998 allocs/op
+BenchmarkGoroutineUsingChannelReturn-8   	   14350	     83810 ns/op	   15829 B/op	     398 allocs/op
+PASS
+ok  	github.com/ragpanda/light-future	3.142s
+```
---
+++ bench_test.go
@@ -0,0 +1,66 @@
+package light_future
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Tmp struct {
+	A int
+	B bool
+	C string
+}
+
+func BenchmarkFuture(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ctx := context.Background()
+
+		var fList []*Future
+		for i := 0; i < 100; i++ {
+			a := i
+			f := NewFuture(ctx, NewClosureRunnable(func(ctx context.Context) (interface{}, error) {
+				return &Tmp{
+					A: a + 10,
+					C: fmt.Sprintf("%d", a*100000),
+				}, nil
+			})).Send()
+			fList = append(fList, f)
+		}
+
+		for k, f := range fList {
+			r, _ := f.Await().Result()
+			rt := r.(*Tmp)
+			assert.Equal(b, k, rt.A-10)
+		}
+
+	}
+}
+
+func BenchmarkGoroutineUsingChannelReturn(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		ctx := context.Background()
+
+		var fList []chan *Tmp
+		for i := 0; i < 100; i++ {
+			chanInt := make(chan *Tmp, 1)
+			go func(ctx context.Context, i int) {
+				b := &Tmp{
+					A: i + 10,
+					C: fmt.Sprintf("%d", i),
+				}
+				chanInt <- b
+			}(ctx, i)
+			fList = append(fList, chanInt)
+
+		}
+
+		for k, f := range fList {
+			r := <-f
+			assert.Equal(b, k, r.A-10)
+		}
+
+	}
+}
